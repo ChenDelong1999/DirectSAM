a1858238ee6f03b863ce56ee00096809524d6510
ChenDelong1999 <673939938@qq.com>
2024-08-23 16:40:01 +0800

    add DirectSAM fine-tuning

--- README.md
+++ README.md
@@ -5,12 +5,12 @@
 cd /home/dchenbs/workspace/DirectSAM
 conda activate subobject
 CUDA_VISIBLE_DEVICES=2,3 torchrun --nproc_per_node 2 --master_port 29510 train.py \
-    --pretrained "nvidia/segformer-b0-finetuned-cityscapes-1024-1024" \
-    --per_device_train_batch_size 64 --gradient_accumulation_steps 1 \
+    --pretrained nvidia/segformer-b0-finetuned-cityscapes-1024-1024 \
+    --per_device_train_batch_size 32 --gradient_accumulation_steps 1 \
     --learning_rate 1e-4 \
     --dataset directsam_pseudo_label \
-    --num_train_epochs 100 \
-    --input_resolution 768 \
+    --num_train_epochs 10 \
+    --input_resolution 1024 --thickness 3 \
     --dataloader_num_workers 16 --dataloader_prefetch_factor 8
     
 ```
@@ -36,16 +36,20 @@ https://www.lvisdataset.org/dataset
 
 ckpts=(
     "chendelong/DirectSAM-1800px-0424"
-    "chendelong/DirectSAM-EntitySeg-1024px-0501"
-    "chendelong/DirectSAM-tiny-distilled-10ep-1024px-0726"
-    "chendelong/DirectSAM-tiny-distilled-7ep-768px-0807"
+    # "chendelong/DirectSAM-EntitySeg-1024px-0501"
+    # "chendelong/DirectSAM-tiny-distilled-10ep-1024px-0726"
     "chendelong/DirectSAM-tiny-distilled-15ep-768px-0821"
-    "/home/dchenbs/workspace/subobjects-dev/DirectSAM/runs/finetune/0822-1154-[directsam_pseudo_label]-768px-from-nvidia_segformer-b0-finetuned-cityscapes-1024-1024/checkpoint-343000"
 )
 
 datasets=(
     # "PascalPanopticParts"
     "EntitySeg"
+    "ADE20k"
+    "COCONut_relabeld_COCO_val"
+    "LoveDA"
+    "PartImageNet++"
+
+    
 )
 
 
@@ -56,7 +60,7 @@ for ckpt in $ckpts; do
     CUDA_VISIBLE_DEVICES=5 python evaluate.py \
         --dataset_name $dataset \
         --directsam_ckpt $ckpt \
-        --resolution 768 --thickness 2 --n_samples 1000 \
+        --resolution 1024 --n_samples 1000 \
         --threshold_steps 0.01 --bin_step 32 --max_num_tokens 256 
 
     done
---
+++ train.py
@@ -0,0 +1,105 @@
+import os
+import json
+import argparse
+import datetime
+from dataclasses import dataclass
+
+import torch
+import torch.distributed as dist
+
+from data.create_dataset import create_dataset
+from transformers import AutoImageProcessor, AutoModelForSemanticSegmentation, TrainingArguments, Trainer
+
+
+@dataclass
+class DataCollatorForDirectSAM:
+
+    image_processor: AutoImageProcessor
+    
+    def __call__(self, features):
+
+        if type(features[0])==dict:
+            image = [feature['image'] for feature in features]
+            label = [feature['label'] for feature in features]
+        elif type(features[0])==tuple:
+            image = [feature[0] for feature in features]
+            label = [feature[1] for feature in features]
+        else:
+            raise ValueError("Invalid input type")
+
+        return self.image_processor(image, label, do_reduce_labels=False, return_tensors='pt')
+
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser(description='Fine-tuning DirectSAM')
+
+    parser.add_argument('--pretrained', type=str, default="chendelong/DirectSAM-1800px-0424")
+    parser.add_argument('--dataset', type=str, help='Name of the training dataset')
+    parser.add_argument('--input_resolution', type=int, default=1024, help='Input resolution for the image processor')
+    parser.add_argument('--learning_rate', type=float, default=1e-4, help='Learning rate for the training')
+    parser.add_argument('--num_train_epochs', type=int, default=100, help='Number of training epochs')
+    parser.add_argument('--max_steps', type=int, default=-1, help='Maximum number of training steps')
+    parser.add_argument('--per_device_train_batch_size', type=int, default=4, help='Batch size for training')
+    parser.add_argument('--gradient_accumulation_steps', type=int, default=1, help='Number of steps for gradient accumulation')
+    parser.add_argument('--dataloader_num_workers', type=int, default=4, help='Number of workers for the dataloader')
+    parser.add_argument('--dataloader_prefetch_factor', type=int, default=4, help='Prefetch factor for the dataloader')
+    parser.add_argument('--logging_steps', type=int, default=1, help='Number of steps for logging')
+    parser.add_argument('--fp16', type=bool, default=True, help='Use fp16 precision')
+    parser.add_argument('--thickness', type=int, default=2, help='Thickness of the boundary')
+
+    args = parser.parse_args()
+    
+    dist.init_process_group(backend='nccl')
+
+    dataset_config = json.load(open('data/dataset_configs.json'))
+
+    dataset = create_dataset(dataset_config[args.dataset], 'train', resolution=args.input_resolution, thickness=args.thickness)
+
+    model = AutoModelForSemanticSegmentation.from_pretrained(args.pretrained, num_labels=1, ignore_mismatched_sizes=True)
+    image_processor = AutoImageProcessor.from_pretrained("chendelong/DirectSAM-1800px-0424", reduce_labels=True)
+
+    image_processor.size['height'] = args.input_resolution
+    image_processor.size['width'] = args.input_resolution
+
+    data_collator = DataCollatorForDirectSAM(image_processor)
+    
+    if torch.distributed.get_rank() == 0:
+        print(model)
+        print(f"Number of parameters: {model.num_parameters()/1e6}M,  trainable parameters: {sum(p.numel() for p in model.parameters() if p.requires_grad)/1e6}M")
+        print(dataset)
+
+    training_args = TrainingArguments(
+        output_dir=f"runs/{args.dataset}/{datetime.datetime.now().strftime('%m%d-%H%M')}-{args.input_resolution}px-from-{args.pretrained.replace('/', '_')}",
+        
+        learning_rate=args.learning_rate,
+        warmup_steps=10000,
+        lr_scheduler_type='cosine',
+
+        num_train_epochs=args.num_train_epochs,
+        max_steps=args.max_steps,
+        per_device_train_batch_size=args.per_device_train_batch_size,
+        gradient_accumulation_steps=args.gradient_accumulation_steps,
+        
+        save_total_limit=20,
+        save_steps=50000,
+        save_strategy="steps",
+
+        dataloader_num_workers=args.dataloader_num_workers,
+        dataloader_prefetch_factor=args.dataloader_prefetch_factor,
+        
+        do_eval=False,
+        logging_steps=args.logging_steps,
+        remove_unused_columns=False,
+        push_to_hub=False,
+        fp16=args.fp16
+    )
+
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=dataset,
+        data_collator=data_collator,
+    )
+
+    trainer.train()
+

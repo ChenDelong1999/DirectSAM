a691bd2d34f180e24661d18a4e6d6e39fb4156a8
ChenDelong1999 <673939938@qq.com>
2024-08-29 18:48:39 +0800

    update evaluation

--- evaluation/metrics.py
+++ evaluation/metrics.py
@@ -4,46 +4,17 @@ import torch
 from torch.nn import functional as F
 
 
-def edge_zero_padding(boundary, offset):
-    boundary[:offset, :] = 0
-    boundary[-offset:, :] = 0
-    boundary[:, :offset] = 0
-    boundary[:, -offset:] = 0
-    return boundary
+def recall_with_tolerance(target, prediction, tolerance):
+    assert tolerance % 2 == 1 and tolerance > 0, f"r ({tolerance}) must be an odd positive integer"
+
+    if np.sum(target) == 0:
+        return 1.0
+
+    target[:tolerance, :] = target[-tolerance:, :] = target[:, :tolerance] = target[:, -tolerance:] = 0
+    prediction[:tolerance, :] = prediction[-tolerance:, :] = prediction[:, :tolerance] = prediction[:, -tolerance:] = 0
 
 
-def calculate_recall_torch(target, predictions, r, device):
+    prediction_blured = cv2.GaussianBlur(prediction.astype(np.float32), (tolerance, tolerance), 0) > 0
+    true_positive = np.sum(target * prediction_blured)
 
-    r = int(r)
-    assert r % 2 == 1 and r > 0
-    target = torch.tensor(target).to(device).float()
-    predictions = torch.tensor(predictions).to(device).float()
-
-    C, H, W = predictions.shape
-    kernel = torch.ones((C, 1, r, r)).to(device)
-
-    predictions_blur = F.conv2d(predictions, kernel, groups=C, padding=r//2)
-    overlap = target * (predictions_blur > 0)
-    recall = overlap.sum(dim=(1, 2)) / target.sum()
-
-    return recall.cpu().numpy()
-
-
-def get_num_tokens(boundary):
-    num_objects, labels = cv2.connectedComponents((1-boundary).astype(np.uint8))
-    return num_objects
-
-
-def get_metrics(target, prob, thresholds, bzp_offset, tolerance, device):
-    target = edge_zero_padding(target, bzp_offset)
-
-    predictions = np.array([prob > threshold for threshold in thresholds])
-
-    all_num_tokens = []
-    for prediction in predictions:
-        num_tokens = get_num_tokens(prediction)
-        all_num_tokens.append(num_tokens)
-
-    all_recall = calculate_recall_torch(target, predictions, tolerance, device)
-
-    return all_num_tokens, all_recall
+    return true_positive / np.sum(target)
--- model/directsam.py
+++ model/directsam.py
@@ -1,17 +1,20 @@
 import torch
 from transformers import AutoModelForSemanticSegmentation, AutoImageProcessor
 import torch.nn as nn
-
+import numpy as np
+import cv2
+from skimage.morphology import skeletonize
 
 class DirectSAM():
 
-    def __init__(self, model_name, resolution, device):
+    def __init__(self, model_name, resolution, threshold=0.5, device='cuda'):
         self.model = AutoModelForSemanticSegmentation.from_pretrained(model_name).to(device).half().eval()
         self.processor = AutoImageProcessor.from_pretrained('chendelong/DirectSAM-1800px-0424')
 
         self.processor.size['height'] = resolution
         self.processor.size['width'] = resolution
         self.resolution = resolution
+        self.threshold = threshold
 
     def __call__(self, image):
         pixel_values = self.processor(image, return_tensors="pt").pixel_values.to(self.model.device).to(self.model.dtype)
@@ -22,5 +25,30 @@ class DirectSAM():
             size=(self.resolution, self.resolution),
             mode="bicubic",
         )
-        probabilities = torch.sigmoid(upsampled_logits).detach().numpy()[0,0]
-        return probabilities
+        probabilities = torch.sigmoid(upsampled_logits)[0,0]
+        boundary = (probabilities > self.threshold).detach().numpy()
+        boundary, num_tokens = self.boundary_post_processing(boundary)
+
+        return boundary, num_tokens
+
+    def boundary_post_processing(self, boundary):
+
+        boundary = skeletonize(boundary)
+        num_objects, labels = cv2.connectedComponents(
+            (1-boundary).astype(np.uint8), 
+            connectivity=4, 
+            )
+        
+        valid_boundary = np.zeros_like(boundary).astype(np.uint8)
+        for i in range(1, num_objects):
+            mask = labels == i
+            
+            mask = cv2.dilate(mask.astype(np.uint8), np.ones((3, 3), np.uint8))
+            mask = cv2.erode(mask, np.ones((3, 3), np.uint8))
+
+            dialated = cv2.dilate(mask.astype(np.uint8), np.ones((2, 2), np.uint8))
+            eroded = cv2.erode(mask, np.ones((2, 2), np.uint8))
+
+            valid_boundary += (dialated - eroded)
+        return valid_boundary > 0, num_objects - 1
+
